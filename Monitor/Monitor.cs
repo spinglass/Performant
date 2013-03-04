fd38e12c64fb636cbb638588175d736c0e1f56c6
Karl Hammarling <karl@hammarling.com>
2013-03-04 22:12:44 +0000

    Hooking-up all commands
    - All commands now stored in State.
    - Sending commands at recommended frequencies (1, 2, 10 or 100Hz).

--- Monitor/Monitor.cs
+++ Monitor/Monitor.cs
@@ -20,8 +20,12 @@ namespace Monitor
             m_State = new State();
 
             m_Quit = false;
+            m_frameCount = 0;
+ 
             m_Thread = new Thread(ThreadProc);
             m_Thread.Name = "Monitor";
+
+            SetupCommandSets();
         }
 
         public void Start()
@@ -45,21 +49,76 @@ namespace Monitor
             return state;
         }
 
-        private void Send()
+        private void SetupCommandSets()
         {
-            CommandSet cmdSet = new CommandSet();
-            cmdSet.Add(m_WorkDistance);
-            cmdSet.Add(m_WorkTime);
-            cmdSet.Add(m_StrokeState);
-            cmdSet.Add(m_WorkoutState);
-            cmdSet.Prepare();
+            m_100HzCommands = new CommandSet();
+            m_100HzCommands.Add(m_StrokeStateCommand);
+            m_100HzCommands.Prepare();
 
-            if (m_Commander.Send(cmdSet))
+            m_10HzCommands = new CommandSet();
+            m_10HzCommands.Add(m_WorkDistanceCommand);
+            m_10HzCommands.Add(m_WorkTimeCommand);
+            m_10HzCommands.Prepare();
+
+            m_2HzCommands = new CommandSet();
+            m_2HzCommands.Add(m_CadenceCommand);
+            m_2HzCommands.Add(m_CaloriesCommand);
+            m_2HzCommands.Add(m_DragFactorCommand);
+            m_2HzCommands.Add(m_PaceCommand);
+            m_2HzCommands.Add(m_PowerCommand);
+            m_2HzCommands.Add(m_WorkoutStateCommand);
+            m_2HzCommands.Prepare();
+
+            m_1HzCommands = new CommandSet();
+            m_1HzCommands.Add(m_HeartRateCommand);
+            m_1HzCommands.Prepare();
+
+            m_PerWorkoutCommands = new CommandSet();
+            m_PerWorkoutCommands.Add(m_WorkoutTypeCommand);
+            m_PerWorkoutCommands.Prepare();
+        }
+
+        private void Send(CommandSet cmdSet)
+        {
+            if (m_Connection.IsOpen)
             {
-                m_State.WorkDistance = m_WorkDistance.WorkDistance;
-                m_State.WorkTime = m_WorkTime.WorkTime;
-                m_State.StrokeState = m_StrokeState.StrokeState;
-                m_State.WorkoutState = m_WorkoutState.WorkoutState;
+                if (m_Commander.Send(cmdSet))
+                {
+                    UpdateState();
+                }
+                else
+                {
+                    if (m_Connection.State != m_ConnectionState)
+                    {
+                        if (m_Connection.State == ConnectionState.SendError)
+                        {
+                            Debug.WriteLine("Connection: send error");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void UpdateState()
+        {
+            lock (m_State)
+            {
+                m_State.ConnectionState = m_Connection.State;
+
+                // PM3 commands
+                m_State.DragFactor = m_DragFactorCommand.DragFactor;
+                m_State.StrokeState = m_StrokeStateCommand.StrokeState;
+                m_State.WorkDistance = m_WorkDistanceCommand.WorkDistance;
+                m_State.WorkoutState = m_WorkoutStateCommand.WorkoutState;
+                m_State.WorkoutType = m_WorkoutTypeCommand.WorkoutType;
+                m_State.WorkTime = m_WorkTimeCommand.WorkTime;
+
+                // CSAFE commands
+                m_State.Calories = m_CaloriesCommand.Calories;
+                m_State.HeartRate = m_HeartRateCommand.HeartRate;
+                m_State.Power = m_PowerCommand.Power;
+                m_State.StrokePace = m_PaceCommand.StrokePace;
+                m_State.StrokeRate = m_CadenceCommand.StrokeRate;
             }
         }
 
@@ -67,54 +126,48 @@ namespace Monitor
         {
             while (!m_Quit)
             {
-                lock (m_State)
+                switch (m_ConnectionState)
                 {
-                    switch (m_ConnectionState)
-                    {
-                        case ConnectionState.Idle:
-                            // Attempt to start the connection
-                            if (m_Connection.Open())
-                            {
-                                Debug.WriteLine("Connection: opened");
+                    case ConnectionState.Idle:
+                        // Attempt to start the connection
+                        if (m_Connection.Open())
+                        {
+                            Debug.WriteLine("Connection: opened");
+                        }
+                        break;
 
-                                m_State.ConnectionState = m_Connection.State;
-                            }
-                            break;
+                    case ConnectionState.Connected:
+                    case ConnectionState.SendError:
+                        
+                        Send(m_100HzCommands);
+                        if (m_frameCount % 10 == 0)
+                        {
+                            Send(m_10HzCommands);
+                        }
+                        if (m_frameCount % 50 == 0)
+                        {
+                            Send(m_2HzCommands);
+                        }
+                        if (m_frameCount % 100 == 0)
+                        {
+                            Send(m_1HzCommands);
+                        }
 
-                        case ConnectionState.Connected:
-                        case ConnectionState.SendError:
-                            Send();
+                        ++m_frameCount;
 
-                            if (m_Connection.IsOpen)
-                            {
-                                if (m_Connection.State != m_ConnectionState)
-                                {
-                                    if (m_Connection.State == ConnectionState.SendError)
-                                    {
-                                        Debug.WriteLine("Connection: send error");
-                                    }
+                        if (!m_Connection.IsOpen)
+                        {
+                            Debug.WriteLine("Connection: lost");
+                        }
+                        break;
 
-                                    m_State.ConnectionState = m_Connection.State;
-                                }
-                            }
-                            else
-                            {
-                                Debug.WriteLine("Connection: lost");
-
-                                m_State.ConnectionState = m_Connection.State;
-                            }
-                            break;
-
-                        case ConnectionState.Lost:
-                            // Attempt to reconnect
-                            if (m_Connection.Reopen())
-                            {
-                                Debug.WriteLine("Connection: re-opened");
-
-                                m_State.ConnectionState = m_Connection.State;
-                            }
-                            break;
-                    }
+                    case ConnectionState.Lost:
+                        // Attempt to reconnect
+                        if (m_Connection.Reopen())
+                        {
+                            Debug.WriteLine("Connection: re-opened");
+                        }
+                        break;
                 }
                 m_ConnectionState = m_Connection.State;
 
@@ -133,11 +186,26 @@ namespace Monitor
         private State m_State;
         private Thread m_Thread;
         private bool m_Quit;
+        private int m_frameCount;
 
         // Commands
-        private WorkDistanceCommand m_WorkDistance = new WorkDistanceCommand();
-        private WorkTimeCommand m_WorkTime = new WorkTimeCommand();
-        private StrokeStateCommand m_StrokeState = new StrokeStateCommand();
-        private WorkoutStateCommand m_WorkoutState = new WorkoutStateCommand();
+        private CadenceCommand m_CadenceCommand = new CadenceCommand();
+        private CaloriesCommand m_CaloriesCommand = new CaloriesCommand();
+        private DragFactorCommand m_DragFactorCommand = new DragFactorCommand();
+        private HeartRateCommand m_HeartRateCommand = new HeartRateCommand();
+        private PaceCommand m_PaceCommand = new PaceCommand();
+        private PowerCommand m_PowerCommand = new PowerCommand();
+        private StrokeStateCommand m_StrokeStateCommand = new StrokeStateCommand();
+        private WorkDistanceCommand m_WorkDistanceCommand = new WorkDistanceCommand();
+        private WorkoutStateCommand m_WorkoutStateCommand = new WorkoutStateCommand();
+        private WorkoutTypeCommand m_WorkoutTypeCommand = new WorkoutTypeCommand();
+        private WorkTimeCommand m_WorkTimeCommand = new WorkTimeCommand();
+
+        // Command sets
+        private CommandSet m_100HzCommands;
+        private CommandSet m_10HzCommands;
+        private CommandSet m_2HzCommands;
+        private CommandSet m_1HzCommands;
+        private CommandSet m_PerWorkoutCommands;
     }
 }
--- Monitor/State.cs
+++ Monitor/State.cs
@@ -14,9 +14,20 @@ namespace Monitor
         }
 
         public ConnectionState ConnectionState = ConnectionState.Idle;
-        public Time WorkTime = new Time();
+
+        // PM3 data
+        public uint DragFactor = 0;
+        public StrokeState StrokeState = StrokeState.Unknown;
         public Distance WorkDistance = new Distance();
         public WorkoutState WorkoutState = WorkoutState.Unknown;
-        public StrokeState StrokeState = StrokeState.Unknown;
+        public WorkoutType WorkoutType = WorkoutType.Unknown;
+        public Time WorkTime = new Time();
+
+        // CSAFE data
+        public uint Calories = 0;
+        public uint HeartRate = 0;
+        public uint Power = 0;
+        public uint StrokePace = 0;
+        public uint StrokeRate = 0;
     }
 }
